23a7f3be8d770c60b4ac67e36349fa9be8aa8bb6
Kamilah <kamilahaviles1821@gmail.com>
2025-10-05 20:38:27 -0400

    responsive design

--- onlineStore/script.js
+++ onlineStore/script.js
@@ -1,1 +1,9 @@
+const bar = document.getElementById('bar');
+const nav = document.getElementById('navbar');
 
+if(bar){ 
+    // if someone clicks on the bar, it will run anonymous function
+    bar.addEventListener('click', () => {
+       nav.classList.add('active'); 
+})
+}
