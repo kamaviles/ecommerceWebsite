5a5a7b06be4e1134045a327d1a6bf896e5ff45d1
Kamilah <kamilahaviles1821@gmail.com>
2025-10-05 21:01:03 -0400

    responsive sidebar menu

--- onlineStore/script.js
+++ onlineStore/script.js
@@ -1,9 +1,17 @@
 const bar = document.getElementById('bar');
 const nav = document.getElementById('navbar');
+const close = document.getElementById('close');
 
 if(bar){ 
     // if someone clicks on the bar, it will run anonymous function
-    bar.addEventListener('click', () => {
-       nav.classList.add('active'); 
-})
+    bar.addEventListener('click', () => { // when clicked, it will add the class 'active' to the nav element
+       nav.classList.add('active'); // this will make the nav visible
+    })
 }
+
+if(close){
+    close.addEventListener('click', () => { // when clicked, it will remove the class 'active' from the nav element
+       nav.classList.remove('active'); // this will make the nav invisible
+    })
+}
+ 
